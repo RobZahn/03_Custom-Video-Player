9762f0d3ad58327160631e1fd17541c7ae5c1f70
Robert Zahn <rob.jzahn@gmail.com>
2020-05-14 11:33:19 -0400

    Implement toggleVideoStatus()

--- dist/script.js
+++ dist/script.js
@@ -4,6 +4,35 @@ const stop = document.querySelector('#stop');
 const progress = document.querySelector('#progress');
 const timestamp = document.querySelector('#timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  return true;
+}
+
+// Update progress & timestamp
+function updateProgress() {
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+}
+
+// Stop video
+function stopVideo() {
+  return true;
+}
+
 // Event listeners
 
 video.addEventListener('click', toggleVideoStatus);
